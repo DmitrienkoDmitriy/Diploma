e9533929679c020608f3e84bf39e59d234304d8b
deera <deera@DESKTOP-G1393V6>
2017-06-21 17:53:58 +0300

    Добавьте файлы проекта.

---
+++ Diploma/MainForm.Designer.cs
@@ -0,0 +1,209 @@
+﻿namespace Diploma
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.файлToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.открытьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.выйтиToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.фільтриToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.періодикаToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.пошукСигналуToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.файлToolStripMenuItem,
+            this.фільтриToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1242, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // файлToolStripMenuItem
+            // 
+            this.файлToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.открытьToolStripMenuItem,
+            this.выйтиToolStripMenuItem});
+            this.файлToolStripMenuItem.Name = "файлToolStripMenuItem";
+            this.файлToolStripMenuItem.Size = new System.Drawing.Size(48, 20);
+            this.файлToolStripMenuItem.Text = "Файл";
+            // 
+            // открытьToolStripMenuItem
+            // 
+            this.открытьToolStripMenuItem.Name = "открытьToolStripMenuItem";
+            this.открытьToolStripMenuItem.Size = new System.Drawing.Size(122, 22);
+            this.открытьToolStripMenuItem.Text = "Відкрити";
+            this.открытьToolStripMenuItem.Click += new System.EventHandler(this.открытьToolStripMenuItem_Click);
+            // 
+            // выйтиToolStripMenuItem
+            // 
+            this.выйтиToolStripMenuItem.Name = "выйтиToolStripMenuItem";
+            this.выйтиToolStripMenuItem.Size = new System.Drawing.Size(122, 22);
+            this.выйтиToolStripMenuItem.Text = "Вийти";
+            this.выйтиToolStripMenuItem.Click += new System.EventHandler(this.выйтиToolStripMenuItem_Click);
+            // 
+            // фільтриToolStripMenuItem
+            // 
+            this.фільтриToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.періодикаToolStripMenuItem,
+            this.пошукСигналуToolStripMenuItem});
+            this.фільтриToolStripMenuItem.Name = "фільтриToolStripMenuItem";
+            this.фільтриToolStripMenuItem.Size = new System.Drawing.Size(63, 20);
+            this.фільтриToolStripMenuItem.Text = "Фільтри";
+            // 
+            // періодикаToolStripMenuItem
+            // 
+            this.періодикаToolStripMenuItem.Name = "періодикаToolStripMenuItem";
+            this.періодикаToolStripMenuItem.Size = new System.Drawing.Size(160, 22);
+            this.періодикаToolStripMenuItem.Text = "Періодика";
+            this.періодикаToolStripMenuItem.Click += new System.EventHandler(this.періодикаToolStripMenuItem_Click);
+            // 
+            // пошукСигналуToolStripMenuItem
+            // 
+            this.пошукСигналуToolStripMenuItem.Name = "пошукСигналуToolStripMenuItem";
+            this.пошукСигналуToolStripMenuItem.Size = new System.Drawing.Size(160, 22);
+            this.пошукСигналуToolStripMenuItem.Text = "Пошук сигналу";
+            this.пошукСигналуToolStripMenuItem.Click += new System.EventHandler(this.пошукСигналуToolStripMenuItem_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // chart1
+            // 
+            this.chart1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.chart1.BackColor = System.Drawing.SystemColors.ControlLight;
+            chartArea2.AxisX.TitleFont = new System.Drawing.Font("Microsoft Sans Serif", 3.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            chartArea2.AxisX2.TitleFont = new System.Drawing.Font("Microsoft Sans Serif", 3.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            chartArea2.AxisY.TitleFont = new System.Drawing.Font("Microsoft Sans Serif", 3.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            chartArea2.AxisY2.TitleFont = new System.Drawing.Font("Microsoft Sans Serif", 3.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            chartArea2.BackColor = System.Drawing.SystemColors.ControlLight;
+            chartArea2.BackImageTransparentColor = System.Drawing.SystemColors.ControlLight;
+            chartArea2.BackSecondaryColor = System.Drawing.SystemColors.ControlLight;
+            chartArea2.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea2);
+            legend2.Enabled = false;
+            legend2.Name = "Legend1";
+            this.chart1.Legends.Add(legend2);
+            this.chart1.Location = new System.Drawing.Point(2, 25);
+            this.chart1.Name = "chart1";
+            this.chart1.Palette = System.Windows.Forms.DataVisualization.Charting.ChartColorPalette.Excel;
+            this.chart1.PaletteCustomColors = new System.Drawing.Color[] {
+        System.Drawing.Color.Black,
+        System.Drawing.Color.Aqua,
+        System.Drawing.Color.DarkGray};
+            series2.ChartArea = "ChartArea1";
+            series2.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series2.LabelBackColor = System.Drawing.Color.Gray;
+            series2.Legend = "Legend1";
+            series2.Name = "Сигнал";
+            this.chart1.Series.Add(series2);
+            this.chart1.Size = new System.Drawing.Size(1026, 386);
+            this.chart1.TabIndex = 3;
+            this.chart1.Text = "chart1";
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.progressBar1.Location = new System.Drawing.Point(12, 417);
+            this.progressBar1.Maximum = 3000;
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(1220, 40);
+            this.progressBar1.TabIndex = 4;
+            // 
+            // listBox1
+            // 
+            this.listBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.listBox1.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(1034, 27);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(198, 381);
+            this.listBox1.TabIndex = 5;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.ClientSize = new System.Drawing.Size(1242, 469);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "MainForm";
+            this.Text = "Дипломна Робота";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem файлToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem открытьToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem выйтиToolStripMenuItem;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.ToolStripMenuItem фільтриToolStripMenuItem;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.ToolStripMenuItem періодикаToolStripMenuItem;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.ToolStripMenuItem пошукСигналуToolStripMenuItem;
+        private System.Windows.Forms.ListBox listBox1;
+    }
+}
+
---
+++ Diploma/MainForm.cs
@@ -0,0 +1,339 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Threading;
+using System.Windows.Forms;
+using System_signal;
+
+namespace Diploma
+{
+    public partial class MainForm : Form
+    {
+        double megatmp = 0;
+        public int maxCountOfPacks = 60;
+        public int minCountOfPacks = 45;
+        public int countOfPeriodicsEvents = 1;
+        bool open = false;
+        Signal signal = new Signal();
+        List<double> ResultSignal = new List<double>();
+        public MainForm()
+        {
+            InitializeComponent();
+            this.MouseWheel += new MouseEventHandler(chData_MouseWheel);
+        }
+        private void выйтиToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+        private void открытьToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            open = true;
+            signal.Dispose();
+            chart1.Series["Сигнал"].Points.Clear();
+            chart1.ChartAreas[0].AxisX.ScaleView.ZoomReset(0);
+            chart1.ChartAreas[0].AxisY.ScaleView.ZoomReset(0);
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                string[] lines = System.IO.File.ReadAllLines(openFileDialog1.FileName);
+                Array.Clear(lines, 0, 1);
+                foreach (string line in lines)
+                {
+                    if (line != null)
+                    {
+                        signal.inputData.Add(new PointF((float)Convert.ToDouble(line.Split('\t')[0]), (float)Convert.ToDouble(line.Split('\t')[1].Replace('.', ','))));
+                    }
+                }
+                for (int i = 0; i < signal.inputData.Count; i++)
+                {
+                    chart1.Series["Сигнал"].Points.AddXY(signal.inputData[i].Y, signal.inputData[i].X);
+                    chart1.Series["Сигнал"].Points[i].Color = Color.Azure;
+                }
+                menuStrip1.Items[1].Visible = true;
+            }
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            menuStrip1.Items[1].Visible = false;
+            chart1.ChartAreas[0].AxisX.ScaleView.Zoomable = true;
+            chart1.ChartAreas[0].BackColor = Color.Gray;
+        }
+        private void chData_MouseWheel(object sender, MouseEventArgs e)
+        {
+            try
+            {
+                if (e.Delta < 0 && open == true)
+                {
+                    double xMin = chart1.ChartAreas[0].AxisX.ScaleView.ViewMinimum;
+                    double xMax = chart1.ChartAreas[0].AxisX.ScaleView.ViewMaximum;
+                    double yMin = chart1.ChartAreas[0].AxisY.ScaleView.ViewMinimum;
+                    double yMax = chart1.ChartAreas[0].AxisY.ScaleView.ViewMaximum;
+
+                    double posXStart = chart1.ChartAreas[0].AxisX.PixelPositionToValue(e.Location.X) - (xMax - xMin) * 2;
+                    double posXFinish = chart1.ChartAreas[0].AxisX.PixelPositionToValue(e.Location.X) + (xMax - xMin) * 2;
+                    double posYStart = chart1.ChartAreas[0].AxisY.PixelPositionToValue(e.Location.Y) - (yMax - yMin) *4;
+                    double posYFinish = chart1.ChartAreas[0].AxisY.PixelPositionToValue(e.Location.Y) + (yMax - yMin) * 4;
+
+                    chart1.ChartAreas[0].AxisX.ScaleView.Zoom(posXStart, posXFinish);
+                    chart1.ChartAreas[0].AxisY.ScaleView.Zoom(posYStart, posYFinish);
+                   
+                    chart1.ChartAreas[0].AxisX.Interval = megatmp;
+                    megatmp /= 2;
+
+                }
+
+                if (e.Delta > 0 && open == true)
+                {
+                    double xMin = chart1.ChartAreas[0].AxisX.ScaleView.ViewMinimum;
+                    double xMax = chart1.ChartAreas[0].AxisX.ScaleView.ViewMaximum;
+
+                    double posXStart = chart1.ChartAreas[0].AxisX.PixelPositionToValue(e.Location.X) - (xMax - xMin) / 4;
+                    double posXFinish = chart1.ChartAreas[0].AxisX.PixelPositionToValue(e.Location.X) + (xMax - xMin) / 4;
+
+                    chart1.ChartAreas[0].AxisX.ScaleView.Zoom(posXStart, posXFinish);
+                    chart1.ChartAreas[0].AxisX.Interval = megatmp;
+
+                    megatmp *= 2;
+                    
+                }
+            }
+            catch { }
+        }//zoom chart
+        public List<double> CosTransform(List<double> input, int numOfPacks)
+        {
+            int N = input.Count / numOfPacks;
+            List<List<double>>output = new List<List<double>>();
+            List<double> resultSum = new List<double>();
+            double phi = Math.PI / 8.0f;
+            double sumOfC = 0;
+            
+            for (int j = 0; j < numOfPacks; j++)
+            {
+                output.Add(new List<double>());
+                for (int i = 0; i < N; i++)
+                {
+                     for (int x = j * N; x < j * N + N; x++)
+                     {
+                         sumOfC += input[x] * Math.Cos((2 * Math.PI * x * i) / N - phi);
+                     }
+                     output[j].Add(sumOfC);
+                     sumOfC = 0;
+                }
+            }
+            for (int i = 0; i < N; i++)
+            {
+                for (int j = 0; j < output.Count; j++)
+                {
+                    sumOfC += output[j][i];
+                }
+                resultSum.Add(sumOfC / numOfPacks);
+                sumOfC = 0;
+            }
+            return resultSum;
+        }
+        #region
+        //public List<double> CosTransform1(List<double> input, int numOfPacks)
+        //{
+        //    int N = input.Count / numOfPacks;
+        //    List<double> output = new List<double>();
+        //    double phi = Math.PI / 8.0f;
+        //    double sumOfC = 0;
+        //    for (int i = 0; i < N; i++)
+        //    {
+        //        for (int x = 0; x < N; x++)
+        //        {
+        //            sumOfC += input[x] * Math.Cos((2 * Math.PI * x * i) / N - phi);
+        //        }
+        //        output.Add(sumOfC);
+        //        sumOfC = 0;
+        //    }
+        //    return output;
+        //}
+        //public double CosTransform(List<double> _input, int startIndex, int N)
+        //{
+        //    List<double> output = new List<double>();
+        //    List<double> input = new List<double>();
+        //    for (int i = startIndex; i < startIndex+N; i++)
+        //    {
+        //        input.Add(_input[i]);
+        //    }
+        //    double phi = Math.PI / 8.0f;
+        //    double sumOfC = 0;
+        //    for (int i = 0; i < N; i++)
+        //    {
+        //        for (int x = 0; x < N; x++)
+        //        {
+        //            sumOfC += input[x] * Math.Cos((2 * Math.PI * x * i)/N - phi);
+        //        }
+        //        output.Add(sumOfC);
+        //        sumOfC = 0;
+        //    }
+        //    for (int i = 0; i < N; i++)
+        //    {
+        //        sumOfC += output[i];
+        //    }
+        //    return sumOfC/N;
+        //}
+        #endregion
+        public List<double> TransformFromCos(List<double> input)
+        {
+            List<double> output = new List<double>();
+            int N = input.Count;
+            double phi = Math.PI / 8.0f;
+            double sumOfC = 0;
+            for (int x = 0; x < N; x++)
+            {
+                for (int i = 0; i < N; i++)
+                {
+                    sumOfC += 2 / (N * Math.Sin(2 * phi)) * input[i] * Math.Sin(2 * Math.PI * x * i / N + phi);
+                }
+                output.Add(sumOfC);
+                sumOfC = 0;
+            }
+            return output;
+        }
+
+        public double findMiddleSquaredError(List<double> OriginalData, List<double> transformedAndComebackAgainData, int numOfPacks)
+        {
+            double error = 0;
+            double sumForError = 0;
+            List<double> errors = new List<double>();
+            for (int i = 0; i < numOfPacks; i++)
+            {
+                for (int j = 0; j < transformedAndComebackAgainData.Count; j++)
+                {
+                    errors.Add(OriginalData[i*transformedAndComebackAgainData.Count + j] - transformedAndComebackAgainData[j]);
+                }
+            }
+            for (int i = 0; i < errors.Count; i++)
+            {
+                sumForError += Math.Pow(errors[i], 2);
+            }
+            error = Math.Sqrt(sumForError) / errors.Count;
+            return error;
+        }
+
+        public void findPeriod(List<double> inputData, int countOftimes)
+        {
+            List<double> output = inputData;
+            if (countOftimes < countOfPeriodicsEvents)
+            {
+                output = new List<double>();
+                double error;
+                double min = double.MaxValue;
+                List<double> resultDataTmp = new List<double>();
+                int tmpNumOfPacks = 0;
+                for (int j = maxCountOfPacks; j > minCountOfPacks; j--)
+                {
+                    int numOfPacks = j;
+
+                    List<double> middledTransformedData = new List<double>();
+                    List<double> resultData = new List<double>();
+                    middledTransformedData = CosTransform(inputData, numOfPacks);
+                    resultData = TransformFromCos(middledTransformedData);
+                    error = findMiddleSquaredError(inputData, resultData, numOfPacks);
+                    if (error < min)
+                    {
+                        resultDataTmp = resultData;
+                        min = error;
+                        tmpNumOfPacks = numOfPacks;
+                    }
+                    progressBar1.Invoke(new Action(() => { progressBar1.Value++; }));
+                }
+                for (int i = 0; i < tmpNumOfPacks; i++)
+                {
+                    for (int j = i * resultDataTmp.Count; j < i * resultDataTmp.Count + resultDataTmp.Count; j++)
+                    {
+                        output.Add(inputData[j] - resultDataTmp[j - resultDataTmp.Count * i]);
+                    }
+                }
+                open = true;
+                chart1.Invoke(new Action(() =>
+                {
+                    chart1.Series["Сигнал"].Points.Clear();
+                    chart1.ChartAreas[0].AxisX.ScaleView.ZoomReset(0);
+                    chart1.ChartAreas[0].AxisY.ScaleView.ZoomReset(0);
+                    for (int j = 0; j < output.Count; j++)
+                    {
+                        chart1.Series["Сигнал"].Points.AddXY(j, output[j]);
+                        chart1.Series["Сигнал"].Points[j].Color = Color.Azure;
+                    }
+                }));
+                countOftimes++;
+                findPeriod(output, countOftimes);
+            }
+            else
+            {
+                ResultSignal = output;
+                return;
+            }
+        }
+        public void func()
+        {
+            List<double> inputData = new List<double>();
+            for (int i = 0; i < signal.inputData.Count; i++)
+            {
+                inputData.Add(signal.inputData[i].X);
+            }
+            findPeriod(inputData, 0);
+        }
+        private void періодикаToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            progressBar1.Maximum = (maxCountOfPacks - minCountOfPacks) * countOfPeriodicsEvents;
+            Thread myThread = new Thread(func);
+            myThread.Start();
+        }
+
+        private void пошукСигналуToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            signal.Dispose();
+            for (int i = 0; i < ResultSignal.Count; i++)
+            {
+               signal.inputData.Add(new PointF((float)ResultSignal[i],i));
+            }
+            foreach (string line in signal.scalarMultiply(signal.inputData))
+            {
+                listBox1.Items.Add(line);
+            }
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            double timeForChart = Convert.ToDouble(listBox1.SelectedItem.ToString().Split('\t')[1]);
+
+            double maxAmplituda = double.MinValue;
+            double minAmplituda = double.MaxValue;
+            for (int i = 0; i < signal.inputData .Count; i++)
+            {
+                if (signal.inputData[i].Y >= timeForChart - 9 && signal.inputData[i].Y <= timeForChart + 9)
+                {
+                    chart1.Series["Сигнал"].Points[i].Color = Color.Red;
+                    if (signal.inputData[i].X < minAmplituda)
+                        minAmplituda = signal.inputData[i].X;
+                    if (signal.inputData[i].X > maxAmplituda)
+                        maxAmplituda = signal.inputData[i].X;
+                }
+            }
+            chart1.ChartAreas[0].AxisY.ScaleView.Zoom(Math.Round(minAmplituda,1), Math.Round(maxAmplituda, 1));
+            chart1.ChartAreas[0].AxisX.ScaleView.Zoom(timeForChart - 20, timeForChart + 20);
+        }
+    } 
+}
+
+
+
+
+
+
+
+
+
+     
+
+
+
+
+
+
+
+ 
